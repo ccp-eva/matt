c3c7455ee34913bcd914b16b065c543cd582e4f9
Noemi Thiede <noemi_thiede@eva.mpg.de>
2022-11-08 13:38:13 +0100

    folder changes populations - Steven

--- src/populations/en-us/translation.js
+++ src/populations/en-us/translation.js
@@ -1,6 +1,6 @@
 export const translation = {
 	camelCase: "Naming Convention is called camelCase",
-	smcWelcome: 'Willkommen und schön, dasss Du dabei bist',
+	siWelcome: 'Willkommen und schön, dasss Du dabei bist',
 	bootRescue: 'Welches Boot rettest du',
 	descisonThis: 'Dieses Boot.',
 	descisonMiddle: 'Ich kann mich nicht entscheiden',
