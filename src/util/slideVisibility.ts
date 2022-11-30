889ac688672fa3b12bdae8117ac79017ff2fe300
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-11-30 12:33:40 +0100

    Update slideVisibility helpers

--- src/util/slideVisibility.ts
+++ src/util/slideVisibility.ts
@@ -1,12 +1,12 @@
-type SvgInHtml = HTMLElement & SVGElement;
+import { SvgInHtml } from '../types';
 
 /**
  * Receives a parent id as string and returns all child ids as string[]
  *
- * @param parentId - The parent element id as string
+ * @param parentId - The parent element id as string (defaults to 'svg')
  * @returns All direct children of the parent element
  */
-export const getChildrenFromParent = (parentId: string) => {
+export const getChildrenFromParent = (parentId = 'svg') => {
 	const parent = document.getElementById(parentId) as SvgInHtml;
 	const childrenArray = [...parent.children];
 	const childIds = childrenArray.map((e) => e.id);
@@ -14,15 +14,39 @@ export const getChildrenFromParent = (parentId: string) => {
 };
 
 /**
- * Hides all direct children slides from a given parent id as string
+ * Hides all direct children from a given parent id as string (defaults to 'svg').
+ * Those are usually groups of you slides. Note that this function
+ * only targets group elements `<g>`.
  *
- * @param parentId - The parent element id as string
+ * @remarks
+ * SVG’s `g` node can also be set to hidden (i.e., `visibility="hidden"`).
+ *
+ * @param parentId The parent element id as string
  * @returns void
  */
-export const hideAllSlides = (parentId: string) => {
+export const hideFirstChildSlides = (parentId = 'svg') => {
 	const children = getChildrenFromParent(parentId);
 	children.forEach((child) => {
 		document.getElementById(child)!.setAttribute('visibility', 'hidden');
+	});
+};
+
+/**
+ * Hides all(!) subsequent children from a given parent id as string (defaults to 'svg').
+ * Note that this function only targets group elements `<g>`.
+ * The parent element itself is not hidden.
+ *
+ * @see {@link hideFirstChildSlides}
+ *
+ * @param parentId The parent element id as string
+ * @returns void
+ */
+export const hideAllChildSlides = (parentId = 'svg') => {
+	const gNodes = document.querySelectorAll('g') as NodeListOf<SVGGElement>;
+	// drop the first element, which is the parent 'svg' element
+	const gChildNodes = [...gNodes].slice(1, gNodes.length);
+	gChildNodes.forEach((child: SVGGElement) => {
+		child.setAttribute('visibility', 'hidden');
 	});
 };
 
@@ -33,8 +57,8 @@ export const hideAllSlides = (parentId: string) => {
  * @returns void
  */
 export const showSingleSlide = (slideId: string) => {
-	hideAllSlides('svg'); // todo make this dynamic?
-	const allSlides = getChildrenFromParent('svg'); // todo make this dynamic?
+	hideFirstChildSlides();
+	const allSlides = getChildrenFromParent();
 	document.getElementById(slideId)!.setAttribute('visibility', 'visible');
 };
 
