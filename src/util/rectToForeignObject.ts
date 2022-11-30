f2e8a6700ab0c52e215b567234607abd44491f2d
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-11-30 15:44:08 +0100

    add rectToForeignObject function
    Automatically converts rect svg elements to foreignObjects while keeping
    the original properties
    
    Code by Julia Prein from gafo
    Co-authored-by: Julia Prein <jprein@me.com>"

---
+++ src/util/rectToForeignObject.ts
@@ -0,0 +1,16 @@
+export const rectToForeignObject = () => {
+	// get all rect nodes that start with id "text-""
+	const foreignObjects = Array.from(document.querySelectorAll('[id^="text-"]'));
+	foreignObjects.forEach((e) => {
+		const obj = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'foreignObject'
+		);
+		// copy all attributes from rect to new foreignObject
+		[...e.attributes].map(({ name, value }) => obj.setAttribute(name, value));
+		e.replaceWith(obj);
+
+		// add default child p element
+		obj.appendChild(document.createElement('p'));
+	});
+};
