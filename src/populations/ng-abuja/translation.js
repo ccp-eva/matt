51f9df9acad15eda126fdea25f49d9942c567379
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-12-01 14:53:14 +0100

    remove placeholder files

--- src/populations/ng-abuja/translation.js
+++
@@ -1,8 +0,0 @@
-export const translation = {
-	camelCase: "Naming Convention is called camelCase",
-	smcWelcome: 'Willkommen und schön, dasss Du dabei bist',
-	bootRescue: 'Welches Boot rettest du',
-	descisonThis: 'Dieses Boot.',
-	descisonMiddle: 'Ich kann mich nicht entscheiden',
-	keySlide: 'Text',
-};
