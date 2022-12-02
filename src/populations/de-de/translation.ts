8741334ccdd142537a3ec92ab0eee3368bd373f1
Noemi Thiede <noemi_thiede@eva.mpg.de>
2022-12-02 17:22:09 +0100

    text replaced with rectangles & json text file

--- src/populations/de-de/translation.ts
+++ src/populations/de-de/translation.ts
@@ -1,13 +1,55 @@
 export const translation = {
 	welcome: '<h1 class="heading">Willkommen und schön,<br>dass Du dabei bist!</h1>',
 	
+	saving: '<p>Rettest Du ...</p>',
+	tenObjects: '<p>10 Stifte</p>',
+	cantDecide: '<p>Ich kann mich nicht entscheiden</p>',
+	oneObject: '<p>1 Fahrrad</p>',
+	confirm: '<p>Wolltest du das aussuchen?</p>',
+	oneHuman: '<p>1 Mensch</p>',
+	twoHumans: '<p>2 Menschen</p>',
+	tenHumans: '<p>10 Menschen</p>',
+	hundredHumans: '<p>100 Menschen</p>',
+	oneComp: '<p>1 Katze</p>',
+	twoComp: '<p>2 Katzen</p>',
+	tenComp: '<p>10 Katzen</p>',
+	hundredComp: '<p>100 Katzen</p>',
+	oneFood: '<p>1 Kuh</p>',
+	twoFood: '<p>2 Kühe</p>',
+	tenFood: '<p>10 Kühe</p>',
+	hundredFood: '<p>100 Kühe</p>',
+	tenObjectOne: '<p>10 Nägel</p>',
+	tenObjectTwo: '<p>10 Schwämme</p>',
 
+	thoughts: '<p>Haben Tiere Gedanken?</p>',
+	thoughtsComp: '<p>Denken Tiere so wie Menschen?</p>',
+	feelings: '<p>Haben Tiere Gefühle?</p>',
+	feelingsComp: '<p>Fühlen Tiere so wie Menschen?</p>',
+	difference: '<p>Sind Menschen anders als Tiere?</p>',
 
+	intelligence: '<p>Wie schlau sind sie?</p>',
+	pain: '<p>Wieviel können sie Schmerzen fühlen?</p>',
+	feelingsRating: '<p>Wie sehr können sie sich glücklich,<br>traurig oder ängstlich fühlen?</p>',
+	similarity: '<p>Wie ähnlich sind sie Menschen?</p>',
 
+	notAtAll: '<p>Überhaupt nicht</p>',
+	aLittle: '<p>Ein bisschen</p>',
+	aMediumAmount: '<p>Mittel</p>',
+	aLotSmart: '<p>Sehr schlau</p>',
+	aLotPain: '<p>Sehr viel</p>',
+	aLot: '<p>Sehr</p>',
+	aLotSimilar: '<p>Sehr ähnlich</p>',
 
+	ownership: '<p>Haben Du und Deine Familie Tiere<br>oder gibt es Tiere bei euch zuhause?</p>',
+	exposure: '<p>Wie oft triffst Du diese Tiere?</p>',
 
-	
-	
+	never: '<p>Nie</p>',
+	sometimes: '<p>Manchmal</p>',
+	often: '<p>Oft</p>',
+	almostEveryDay: '<p>Fast jeden Tag</p>',
+
+	meatFish: '<p>Isst Du Fleisch oder Fisch?</p>',
+	eggCheeseMilk: '<p>Isst Du Eier, Käse oder trinkst Du Milch?</p>',
 	
 	thankYou: '<h1 class="heading">Vielen Dank!</h1>'
 };
