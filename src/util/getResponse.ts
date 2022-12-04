fb07c1578b0dd06c30ba8b8122d6ed4f614007f3
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-12-04 17:03:50 +0100

    allow getResponse to receive a parameter on which click event fires

--- src/util/getResponse.ts
+++ src/util/getResponse.ts
@@ -1,11 +1,17 @@
-export const getResponse = () => {
+export const getResponse = (id?: string) => {
 	return new Promise<MouseEvent | TouchEvent>((resolve) => {
 		const handleResponse = (event: MouseEvent | TouchEvent) => {
 			return resolve(event);
 		};
 
-		document
-			.getElementById('wrapper')!
-			.addEventListener('click', handleResponse, { once: true });
+		if (id) {
+			document
+				.getElementById(id)!
+				.addEventListener('click', handleResponse, { once: true });
+		} else {
+			document
+				.getElementById('wrapper')!
+				.addEventListener('click', handleResponse, { once: true });
+		}
 	});
 };
