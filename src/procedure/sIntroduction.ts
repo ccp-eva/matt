9eb6e70924ba17e9e0ffec1cdd2e321e89ff3d16
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-01-04 13:25:12 +0100

    Update path

---
+++ src/procedure/sIntroduction.ts
@@ -0,0 +1,44 @@
+import { gsap } from 'gsap';
+import { play, playPromise } from '../util/audio';
+import { swapSlides } from '../util/slideVisibility';
+import siWelcome from '../cultures/deUrban/audio/si-welcome_1.mp3';
+import siHeadphones from '../cultures/deUrban/audio/si-headphones_1.mp3';
+import siNextRed from '../cultures/deUrban/audio/si-next-red_1.mp3';
+import { getResponse } from '../util/getResponse';
+
+export const sIntroduction = async () => {
+	// show first slide
+	swapSlides(['s-introduction']);
+
+	const tl = gsap.timeline();
+	const nextButton = document.getElementById('link-si-next')!;
+	gsap.set(nextButton, {
+		transformOrigin: '50% 50%',
+		opacity: 0,
+		visibility: 'hidden',
+	});
+
+	tl.to(nextButton, {
+		delay: 8,
+		duration: 0.5,
+		opacity: 1,
+		visibility: 'visible',
+	});
+	tl.to(nextButton, {
+		filter: 'drop-shadow(0px 0px 14px #a90707)',
+		delay: 1,
+		repeat: -1,
+		yoyo: true,
+		reversed: true,
+	});
+
+	await playPromise(siWelcome);
+	await playPromise(siHeadphones);
+	await playPromise(siNextRed);
+
+	play(siNextRed, 'link-si-headphones');
+
+	await getResponse('link-si-next', true);
+
+	swapSlides(['s-moral-circle'], ['s-introduction']);
+};
