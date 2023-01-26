8c3d9712bcb0571cede6320c5e687f7dbc9a0aaa
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-01-26 13:06:34 +0100

    make robust

--- src/util/styleDefaults.ts
+++ src/util/styleDefaults.ts
@@ -4,29 +4,33 @@ import { gsap } from 'gsap';
 export const setMousePointer = () => {
 	config.css.mousePointerObjects.forEach((e: string) => {
 		const element = document.getElementById(e)!;
-		element.style.cursor = 'pointer';
+		if (element) {
+			element.style.cursor = 'pointer';
+		}
 	});
 };
 
 export const setScaleOnHover = () => {
 	config.css.scaleOnHoverObjects.forEach((e: string) => {
 		const element = document.getElementById(e)!;
-		gsap.set(element, { transformOrigin: '50% 50%' });
-		let tween = gsap.to(element, {
-			scale: 1.2,
-			ease: 'none',
-			paused: true,
-		});
-
-		element.addEventListener('mouseenter', () => {
-			gsap.to(tween, {
-				duration: 1.3,
-				time: tween.duration(),
-				ease: 'elastic.out(0.8, 0.3)',
+		if (element) {
+			gsap.set(element, { transformOrigin: '50% 50%' });
+			let tween = gsap.to(element, {
+				scale: 1.2,
+				ease: 'none',
+				paused: true,
 			});
-		});
-		element.addEventListener('mouseleave', () => {
-			gsap.to(tween, { duration: 0.1, time: 0, ease: 'none', overwrite: true });
-		});
+
+			element.addEventListener('mouseenter', () => {
+				gsap.to(tween, {
+					duration: 1.3,
+					time: tween.duration(),
+					ease: 'elastic.out(0.8, 0.3)',
+				});
+			});
+			element.addEventListener('mouseleave', () => {
+				gsap.to(tween, { duration: 0.1, time: 0, ease: 'none', overwrite: true });
+			});
+		}
 	});
 };
