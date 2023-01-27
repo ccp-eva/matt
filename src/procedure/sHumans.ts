f3a436c32ebcc5b1f23a700fbc2028faa271d8aa
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-01-27 13:10:31 +0100

    change old slide name

--- src/procedure/sHumans.ts
+++ src/procedure/sHumans.ts
@@ -5,7 +5,7 @@ export default async () => {
 	data.slideCount++;
 
 	// show first slide
-	swapSlides('s-humans', 's-moral-circle');
+	swapSlides('s-humans', 's-transition-1');
 
 	await playPromise(`./cultures/${data.culture}/audio/s-humans_1.mp3`);
 };
