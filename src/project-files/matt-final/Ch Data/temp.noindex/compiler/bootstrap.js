db9c0c1a8776244546644918c31f808420c9f1d5
Noemi Thiede <noemi_thiede@eva.mpg.de>
2023-02-06 14:08:37 +0100

    added character animator project file to folder project files

---
+++ src/project-files/matt-final/Ch Data/temp.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = _modulePath;
+var __compiledScene = _cwd + "/scene.json";
+runScript("/Applications/Adobe Character Animator 2023/Adobe Character Animator 2023.app/Contents/Resources/Runtime/animal/scripts/js/lib/engine.js")
