e55c50e38ea4cfee24beada69223b879ab8092f9
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-02-12 13:43:33 +0100

    use dedicated autio element to have control over active played audio

--- src/util/audio.ts
+++ src/util/audio.ts
@@ -1,39 +1,42 @@
 export const playPromise = (url: string, elementId?: string) => {
-	if (elementId) {
-		return new Promise(function (resolve, reject) {
+	const audio = document.getElementById('audio') as HTMLAudioElement;
+
+	return new Promise(function (resolve, reject) {
+		if (elementId) {
 			document.getElementById(elementId)!.addEventListener('click', () => {
-				const audio = new Audio(); // create audio w/o src
-				audio.preload = 'auto'; // intend to play through
-				audio.autoplay = true; // autoplay when loaded
-				audio.onerror = reject; // on error, reject
-				audio.onended = resolve; // when done, resolve
-				audio.src = url;
+				audio.autoplay = true;
+				audio.setAttribute('src', url);
+				audio.onerror = reject;
+				audio.onended = resolve;
 			});
-		});
-	} else {
-		return new Promise(function (resolve, reject) {
-			const audio = new Audio(); // create audio w/o src
-			audio.preload = 'auto'; // intend to play through
-			audio.autoplay = true; // autoplay when loaded
-			audio.onerror = reject; // on error, reject
-			audio.onended = resolve; // when done, resolve
-			audio.src = url;
-		});
-	}
+		} else {
+			audio.autoplay = true;
+			audio.setAttribute('src', url);
+			audio.onerror = reject;
+			audio.onended = resolve;
+		}
+	});
 };
 
 export const play = (url: string, elementId?: string) => {
-	const audio = new Audio(url);
+	const audio = document.getElementById('audio') as HTMLAudioElement;
 
 	if (elementId) {
 		document.getElementById(elementId)!.addEventListener('click', () => {
+			audio.autoplay = true;
 			// Restart the audio by setting the current time to 0
 			audio.currentTime = 0;
 			audio.play();
+			audio.setAttribute('src', url);
 		});
 	} else {
-		audio.preload = 'auto'; // intend to play through
 		audio.autoplay = true; // autoplay when loaded
-		audio.src = url;
+		audio.setAttribute('src', url);
 	}
+};
+
+export const stop = () => {
+	const audio = document.getElementById('audio') as HTMLAudioElement;
+	audio.pause();
+	audio.currentTime = 0;
 };
