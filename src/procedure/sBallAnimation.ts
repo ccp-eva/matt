2f6a459021b810e0e647e72576226ec7645c7ff8
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-06 10:25:22 +0100

    use variables in timeline

--- src/procedure/sBallAnimation.ts
+++ src/procedure/sBallAnimation.ts
@@ -1,4 +1,5 @@
 import { gsap } from 'gsap';
+import { SvgInHtml } from '../types';
 import { playPromise } from '../util/audio';
 import { sleep } from '../util/helpers';
 import { swapSlides } from '../util/slideVisibility';
@@ -8,7 +9,12 @@ export default async () => {
 	swapSlides('s-ball-animation', 's-mc-intro');
 
 	data.procedure.ballAnimation = { completed: false };
-	const ball = document.getElementById('link-sba-ball');
+	const ball = document.getElementById('link-sba-ball')! as SvgInHtml;
+	const innerCircle = document.getElementById('sba-inner')! as SvgInHtml;
+	const middleCircle = document.getElementById('sba-middle')! as SvgInHtml;
+	const outerCircle = document.getElementById('sba-outer')! as SvgInHtml;
+
+	gsap.set([innerCircle, middleCircle, outerCircle], { opacity: 0.5 });
 
 	gsap.set(ball, { autoAlpha: 0 });
 
@@ -24,6 +30,14 @@ export default async () => {
 			duration: 1,
 			x: -575,
 		})
+		.to(
+			middleCircle,
+			{
+				duration: 1,
+				opacity: 1,
+			},
+			'<.5'
+		)
 		.to(ball, {
 			delay: 1,
 			autoAlpha: 0,
@@ -40,6 +54,14 @@ export default async () => {
 			duration: 1,
 			x: -460,
 		})
+		.to(
+			outerCircle,
+			{
+				duration: 1,
+				opacity: 1,
+			},
+			'<.5'
+		)
 		.to(ball, {
 			delay: 1,
 			autoAlpha: 0,
@@ -58,7 +80,15 @@ export default async () => {
 			onComplete: () => {
 				data.procedure.ballAnimation.completed = true;
 			},
-		});
+		})
+		.to(
+			innerCircle,
+			{
+				duration: 1,
+				opacity: 1,
+			},
+			'<.5'
+		);
 
 	await playPromise(`./cultures/${data.culture}/audio/s-ball-animation.mp3`);
 	await playPromise(`./cultures/${data.culture}/audio/sba-middle.mp3`);
