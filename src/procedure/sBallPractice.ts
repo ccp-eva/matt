7e0ad0823346b0d91bf94e428c4a3a3dbf4fd940
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-06 10:25:50 +0100

    add practice trial

--- src/procedure/sBallPractice.ts
+++ src/procedure/sBallPractice.ts
@@ -1,5 +1,6 @@
 import { gsap } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
+import _ from 'lodash';
 import { SvgInHtml } from '../types';
 gsap.registerPlugin(Draggable);
 import { play, playPromise } from '../util/audio';
@@ -10,25 +11,180 @@ export default async () => {
 	data.slideCount++;
 	swapSlides('s-ball-practice', 's-ball-animation');
 
+	data.procedure.ballPractice = { completed: false };
 	const ball = document.getElementById('link-sbp-ball')! as SvgInHtml;
-	Draggable.create(ball, {
-		onDragEnd: function () {
-			console.log('drag end');
-			play(`./cultures/${data.culture}/audio/sbp-resp1_3.mp3`);
-		},
-	});
+	const inner = document.getElementById('sbp-inner')! as SvgInHtml;
+	const middle = document.getElementById('sbp-middle')! as SvgInHtml;
+	const outer = document.getElementById('sbp-outer')! as SvgInHtml;
 
-	await playPromise(`./cultures/${data.culture}/audio/s-ball-practice_3.mp3`);
-	await playPromise(`./cultures/${data.culture}/audio/sbp-medium_1.mp3`);
-	await playPromise(`./cultures/${data.culture}/audio/sbp-expl_1.mp3`);
+	await playPromise(`./cultures/${data.culture}/audio/s-ball-practice.mp3`);
+	await playPromise(`./cultures/${data.culture}/audio/sbp-expl.mp3`);
 
-	await sleep(2000);
+	// practice should be in random order
+	const order = _.shuffle(['inner', 'middle', 'outer']);
+	data.procedure.ballPractice.order = order;
+	data.procedure.ballPractice.inner = '';
+	data.procedure.ballPractice.middle = '';
+	data.procedure.ballPractice.outer = '';
 
-	await playPromise(`./cultures/${data.culture}/audio/sbp-inner_1.mp3`);
+	const startTime = new Date().getTime();
 
-	await sleep(2000);
+	// if first time, init explanationCount
+	if (!data.procedure.ballPractice!.explanationCount) {
+		data.procedure.ballPractice!.explanationCount = 1;
+	}
 
-	await playPromise(`./cultures/${data.culture}/audio/sbp-outer_1.mp3`);
+	let failed = false;
+	let repeat = false;
+	for (let i = 0; i < order.length; i++) {
+		gsap.to([inner, middle, outer], { opacity: 0.5 });
+		gsap.to(ball, { x: 0, duration: 0.25 });
+		const currentCircle = order[i];
+		if (i > 0) {
+			await sleep(1000);
+		}
 
-	await sleep(2000);
+		if (failed) {
+			await playPromise(`./cultures/${data.culture}/audio/sbp-fail.mp3`);
+			await playPromise(`./cultures/${data.culture}/audio/sba-${currentCircle}.mp3`);
+			failed = false;
+			repeat = true;
+		}
+
+		await playPromise(`./cultures/${data.culture}/audio/sbp-${currentCircle}.mp3`);
+		play(`./cultures/${data.culture}/audio/sbp-${currentCircle}.mp3`, 'link-s-bp-headphones');
+
+		Draggable.create(ball, {
+			onDrag: function () {
+				if (
+					this.hitTest(inner, '50%') &&
+					this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					gsap.to(inner, { opacity: 1, duration: 0.25 });
+				} else {
+					gsap.to(inner, { opacity: 0.5, duration: 0.25 });
+				}
+
+				if (
+					!this.hitTest(inner, '50%') &&
+					this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					gsap.to(middle, { opacity: 1, duration: 0.25 });
+				} else {
+					gsap.to(middle, { opacity: 0.5, duration: 0.25 });
+				}
+
+				if (
+					!this.hitTest(inner, '50%') &&
+					!this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					gsap.to(outer, { opacity: 1, duration: 0.25 });
+				} else {
+					gsap.to(outer, { opacity: 0.5, duration: 0.25 });
+				}
+			},
+			onDragEnd: function () {
+				if (
+					this.hitTest(inner, '50%') &&
+					this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					// Logging
+					data.procedure.ballPractice![currentCircle] = 'inner';
+
+					// SUCCESS
+					if (i === 2 && currentCircle === 'inner') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp3.mp3`);
+					}
+
+					if (i < 2 && currentCircle === 'inner') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp${_.random(1, 2)}.mp3`);
+					}
+
+					// FAIL
+					if (currentCircle !== 'inner') {
+						data.procedure.ballPractice.explanationCount!++;
+						failed = true;
+						data.procedure.ballPractice[currentCircle] === '';
+						i -= 1;
+					}
+				}
+				if (
+					!this.hitTest(inner, '50%') &&
+					this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					// Logging
+					data.procedure.ballPractice![currentCircle] = 'middle';
+
+					// SUCCESS
+					if (i === 2 && currentCircle === 'middle') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp3.mp3`);
+					}
+
+					if (i < 2 && currentCircle === 'middle') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp${_.random(1, 2)}.mp3`);
+					}
+
+					// FAIL
+					if (currentCircle !== 'middle') {
+						data.procedure.ballPractice.explanationCount!++;
+						failed = true;
+						data.procedure.ballPractice[currentCircle] === '';
+						i -= 1;
+					}
+				}
+				if (
+					!this.hitTest(inner, '50%') &&
+					!this.hitTest(middle, '50%') &&
+					this.hitTest(outer, '50%')
+				) {
+					// Logging
+					data.procedure.ballPractice![currentCircle] = 'outer';
+
+					// SUCCESS
+					if (i === 2 && currentCircle === 'outer') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp3.mp3`);
+					}
+
+					if (i < 2 && currentCircle === 'outer') {
+						repeat = false;
+						play(`./cultures/${data.culture}/audio/sbp-resp${_.random(1, 2)}.mp3`);
+					}
+
+					// FAIL
+					if (currentCircle !== 'outer') {
+						data.procedure.ballPractice.explanationCount!++;
+						failed = true;
+						data.procedure.ballPractice[currentCircle] === '';
+						i -= 1;
+					}
+				}
+			},
+		});
+
+		while (data.procedure.ballPractice[currentCircle] === '' || repeat) {
+			await sleep(500);
+		}
+	}
+
+	if (
+		data.procedure.ballPractice.inner &&
+		data.procedure.ballPractice.middle &&
+		data.procedure.ballPractice.outer
+	) {
+		data.procedure.ballPractice.completed = true;
+	}
+
+	await sleep(1000);
+
+	data.procedure.ballPractice!.duration = new Date().getTime() - startTime;
 };
