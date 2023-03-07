a8b3783a4b4a497105576757ecba8a67b35d005f
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-07 10:39:39 +0100

    Use querySelectorAll instead of providng single ids in config

--- src/util/styleDefaults.ts
+++ src/util/styleDefaults.ts
@@ -2,34 +2,36 @@ import config from '../config.yaml';
 import { gsap } from 'gsap';
 
 export const setMousePointer = () => {
-	config.css.mousePointerObjects.forEach((e: string) => {
-		const element = document.getElementById(e)!;
-		if (element) {
-			element.style.cursor = 'pointer';
+	config.css.mousePointerObjects.forEach((query: string) => {
+		const elements = document.querySelectorAll<HTMLElement>(query);
+		if (elements) {
+			elements.forEach((e) => (e.style.cursor = 'pointer'));
 		}
 	});
 };
 
 export const setScaleOnHover = () => {
-	config.css.scaleOnHoverObjects.forEach((e: string) => {
-		const element = document.getElementById(e)!;
-		if (element) {
-			gsap.set(element, { transformOrigin: '50% 50%' });
-			let tween = gsap.to(element, {
-				scale: 1.2,
-				ease: 'none',
-				paused: true,
-			});
-
-			element.addEventListener('mouseenter', () => {
-				gsap.to(tween, {
-					duration: 1.3,
-					time: tween.duration(),
-					ease: 'elastic.out(0.8, 0.3)',
+	config.css.scaleOnHoverObjects.forEach((query: string) => {
+		const elements = document.querySelectorAll<HTMLElement>(query);
+		if (elements) {
+			elements.forEach((e) => {
+				gsap.set(e, { transformOrigin: '50% 50%' });
+				let tween = gsap.to(e, {
+					scale: 1.2,
+					ease: 'none',
+					paused: true,
 				});
-			});
-			element.addEventListener('mouseleave', () => {
-				gsap.to(tween, { duration: 0.1, time: 0, ease: 'none', overwrite: true });
+
+				e.addEventListener('mouseenter', () => {
+					gsap.to(tween, {
+						duration: 1.3,
+						time: tween.duration(),
+						ease: 'elastic.out(0.8, 0.3)',
+					});
+				});
+				e.addEventListener('mouseleave', () => {
+					gsap.to(tween, { duration: 0.1, time: 0, ease: 'none', overwrite: true });
+				});
 			});
 		}
 	});
