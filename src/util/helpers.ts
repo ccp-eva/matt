75476af18942a3738d16aa9bd0d725a51efb1e78
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-16 12:21:36 +0100

    Add moveToCenterAnchor
    This centers and SvgInHtml element to the provided x/y
    center coordianates rather than the top left coordiante pair

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,4 +1,5 @@
 import config from '../config.yaml';
+import { SvgInHtml } from '../types';
 
 // promised based timeout
 export const sleep = (ms = 2000) => new Promise<number>((r) => setTimeout(r, ms));
@@ -20,4 +21,16 @@ export const getUrlParameters = () => {
 	}
 
 	return params;
+};
+
+export const moveToCenterAnchor = (svg: SvgInHtml, x: number, y: number) => {
+	// Get the bounding box of the svg
+	const bbox = svg.getBBox();
+
+	// Calculate the center coordinates
+	const cx = bbox.x + bbox.width / 2;
+	const cy = bbox.y + bbox.height / 2;
+
+	// Translate the SVG to the new position
+	svg.setAttribute('transform', `translate(${x - cx}, ${y - cy})`);
 };
