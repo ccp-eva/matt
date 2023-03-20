481a44a61ec058a6edda0ddc4405aa57ac0d2a0d
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-20 08:50:02 +0100

    use subject id as filename for audio input

--- src/procedure/sMeaning.ts
+++ src/procedure/sMeaning.ts
@@ -1,5 +1,6 @@
 import gsap from 'gsap';
 import _ from 'lodash';
+import RecordRTC from 'recordrtc';
 import { SvgInHtml } from '../types';
 import { play, playPromise } from '../util/audio';
 import { getResponse } from '../util/getResponse';
@@ -7,18 +8,6 @@ import { sleep } from '../util/helpers';
 import { swapSlides } from '../util/slideVisibility';
 
 export default async () => {
-	// swap slides automatically (don’t touch this)
-	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide));
-
-	data.procedure.sMeaning = {
-		duration: 0,
-		textInput: '',
-	};
-
-	const nextButton = document.getElementById('link-sm-next') as SvgInHtml;
-	nextButton.style.pointerEvents = 'none';
-	gsap.set(nextButton, { autoAlpha: 0.25 });
-
 	const pinda = document.getElementById('player') as HTMLVideoElement;
 	gsap.set(pinda, { autoAlpha: 0 });
 	const timeline = gsap.timeline();
@@ -34,7 +23,24 @@ export default async () => {
 			delay: 11,
 		});
 
+	console.log(data.currentSlide);
+	console.log(data.previousSlide);
+
+	// await sleep(8000);
+
+	// swap slides automatically (don’t touch this)
+	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide), [10, 1]);
+
 	play(`./cultures/${data.culture}/audio/st-meaning.mp3`, 'link-sm-headphones');
+
+	data.procedure.sMeaning = {
+		duration: 0,
+		textInput: '',
+	};
+
+	const nextButton = document.getElementById('link-sm-next') as SvgInHtml;
+	nextButton.style.pointerEvents = 'none';
+	gsap.set(nextButton, { autoAlpha: 0.25 });
 
 	// get svg rect element
 	const rect = document.getElementById('sm-written-response')! as SvgInHtml;
@@ -44,14 +50,99 @@ export default async () => {
 	fo.removeAttribute('fill');
 	rect.replaceWith(fo);
 	// create textarea
-	const textarea = document.createElement('textarea');
-	fo.appendChild(textarea);
-	textarea.id = 'text-response';
-	textarea.maxLength = 2000;
 
-	textarea.addEventListener('input', () => {
+	fo.innerHTML = `
+	<div id="wrapper">
+	<div id="toggle" style="width: 9em; margin: 0 auto; margin-bottom: 10px">
+		<input id="chck" type="checkbox" />
+		<label for="chck" class="check-trail"><span class="check-handler"></span> </label>
+	</div>
+
+	<div id="response-area">
+		<textarea
+			id="text-response"
+			maxlength="2000"
+			style="pointer-events: auto;"
+		></textarea>
+		<button type="button" id="voice-response" style="display: none; font-size: 2rem; margin: 7% auto; pointer-events: auto; opacity: 1; visibility: inherit;">🎤 Record</button>
+	</div>
+</div>
+`;
+
+	const textResponse = document.getElementById('text-response') as HTMLTextAreaElement;
+	const voiceResponse = document.getElementById('voice-response') as HTMLTextAreaElement;
+
+	// unchecked = keyboard response
+	// checked = voice response
+	const chck = document.getElementById('chck') as HTMLInputElement;
+	chck.addEventListener('change', () => {
+		if (chck.checked) {
+			gsap
+				.timeline()
+				.to(textResponse, {
+					autoAlpha: 0,
+					duration: 0.25,
+					pointerEvents: 'none',
+					display: 'none',
+				})
+				.to(
+					voiceResponse,
+					{
+						autoAlpha: 1,
+						duration: 0.25,
+						pointerEvents: 'auto',
+						display: 'block',
+					},
+					'>'
+				);
+		}
+
+		if (!chck.checked) {
+			gsap
+				.timeline()
+				.to(voiceResponse, {
+					autoAlpha: 0,
+					duration: 0.25,
+					pointerEvents: 'none',
+					display: 'none',
+				})
+				.to(
+					textResponse,
+					{
+						autoAlpha: 1,
+						duration: 0.25,
+						pointerEvents: 'auto',
+						display: 'block',
+					},
+					'>'
+				);
+		}
+	});
+
+	voiceResponse.addEventListener('click', async () => {
+		let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		let recorder = new RecordRTC.RecordRTCPromisesHandler(stream, {
+			type: 'audio',
+		});
+		recorder.startRecording();
+
+		voiceResponse.innerText = '🛑 Stop Recording';
+
+		voiceResponse.addEventListener('click', async () => {
+			await recorder.stopRecording();
+			let blob = await recorder.getBlob();
+			RecordRTC.invokeSaveAsDialog(blob, data.id);
+
+			nextButton.style.pointerEvents = 'auto';
+			gsap.timeline().to(nextButton, {
+				autoAlpha: 1,
+			});
+		});
+	});
+
+	textResponse.addEventListener('input', () => {
 		// show next button if textarea hast at least n chars
-		if (textarea.value.length >= 10) {
+		if (textResponse.value.length >= 15) {
 			nextButton.style.pointerEvents = 'auto';
 			gsap.timeline().to(nextButton, {
 				autoAlpha: 1,
@@ -67,7 +158,7 @@ export default async () => {
 	// save responses and store to response object
 	const response = await getResponse(nextButton.id);
 	console.log(response.id);
-	data.procedure.sMeaning.textInput = textarea.value;
+	data.procedure.sMeaning.textInput = textResponse.value;
 	console.log(data.procedure.sMeaning.textInput);
 
 	await sleep(500);
