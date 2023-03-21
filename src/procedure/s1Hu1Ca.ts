486d5ee830c286405c4de8c09f9bd361d53bddd1
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-21 07:54:38 +0100

    Add Dilemmas

---
+++ src/procedure/s1Hu1Ca.ts
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import { SvgInHtml } from '../types';
+import { play, playPromise } from '../util/audio';
+import { getResponse } from '../util/getResponse';
+import { sleep } from '../util/helpers';
+import { swapSlides } from '../util/slideVisibility';
+
+export default async () => {
+	// swap slides automatically (don’t touch this)
+	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide));
+
+	const left = document.getElementById('s1h1cat-oneHuman')! as SvgInHtml;
+	const center = document.getElementById('s1h1cat-cantDecide')! as SvgInHtml;
+	const right = document.getElementById('s1h1cat-oneCat')! as SvgInHtml;
+
+	play(`./cultures/${data.culture}/audio/s1h1cat-left.mp3`, 'link-s1h1cat-headphones');
+	await playPromise(`./cultures/${data.culture}/audio/s1h1cat-left.mp3`);
+	await playPromise(`./cultures/${data.culture}/audio/saving.mp3`);
+
+	[left, center, right].forEach((el) => {
+		el.classList.add('dilemma-card');
+	});
+
+	// save responses and store to response object
+	let response = await getResponse(['s1h1cat-oneHuman', 's1h1cat-cantDecide', 's1h1cat-oneCat']);
+
+	// bubble up until first g element
+	while (response.tagName !== 'g') {
+		response = response.parentElement!;
+	}
+
+	console.log(response.id);
+	data.procedure[data.currentSlide].response = response.id;
+
+	await sleep(500);
+};
