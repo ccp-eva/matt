ff51517e6e3730eb17784e1a18fe3494fe780e9a
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-21 03:05:30 +0100

    cleanup translations

--- src/translations.ts
+++ src/translations.ts
@@ -1,407 +1,206 @@
 export const translations = {
 	welcome: {
 		'de-urban': '<h1 class="heading">Willkommen und schön,<br>dass Du dabei bist!</h1>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	meaning: {
 		'de-urban': '<h1 class="heading-em">Was heißt es für dich, wenn etwas sehr wichtig ist?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	saving: {
 		'de-urban': '<p class="dilemma__saving">Rettest Du ...</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	tenPencils: {
 		'de-urban': '<p class="dilemma">10 Stifte</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	cantDecide: {
 		'de-urban': '<p class="dilemma">Ich kann mich nicht entscheiden</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	questionMark: {
 		'de-urban': '<p class="dilemma__qm">?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	oneBike: {
 		'de-urban': '<p class="dilemma">1 Fahrrad</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	confirm: {
 		'de-urban': '<p class="dilemma__confirm">Wolltest du das aussuchen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	oneHuman: {
-		'de-urban': '<p>1 Mensch</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">1 Mensch</p>',
 	},
 
 	twoHumans: {
-		'de-urban': '<p>2 Menschen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">2 Menschen</p>',
 	},
 
 	tenHumans: {
-		'de-urban': '<p>10 Menschen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">10 Menschen</p>',
 	},
 	hundredHumans: {
-		'de-urban': '<p>100 Menschen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">100 Menschen</p>',
 	},
 
 	oneCat: {
-		'de-urban': '<p>1 Katze</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">1 Katze</p>',
 	},
 
 	twoCats: {
-		'de-urban': '<p>2 Katzen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">2 Katzen</p>',
 	},
 
 	tenCats: {
-		'de-urban': '<p>10 Katzen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">10 Katzen</p>',
 	},
 
 	hundredCats: {
-		'de-urban': '<p>100 Katzen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">100 Katzen</p>',
 	},
 
 	oneCow: {
-		'de-urban': '<p>1 Kuh</p>',
-		deRural: '',
-		enUrban: '',
-		enRural: '',
+		'de-urban': '<p class="dilemma">1 Kuh</p>',
 	},
 
 	twoCows: {
-		'de-urban': '<p>2 Kühe</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">2 Kühe</p>',
 	},
 
 	tenCows: {
-		'de-urban': '<p>10 Kühe</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">10 Kühe</p>',
 	},
 
 	hundredCows: {
-		'de-urban': '<p>100 Kühe</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
+		'de-urban': '<p class="dilemma">100 Kühe</p>',
 	},
 
 	tenNails: {
-		'de-urban': '<p>10 Nägel</p>',
-		deRural: '',
-		enUrban: '',
-		enRural: '',
+		'de-urban': '<p class="dilemma">10 Nägel</p>',
 	},
 
 	tenSponges: {
-		'de-urban': '<p>10 Schwämme</p>',
-		deRural: '',
-		enUrban: '',
-		enRural: '',
+		'de-urban': '<p class="dilemma">10 Schwämme</p>',
 	},
 
 	thoughtsAdult: {
 		'de-urban': '<p>Haben Tiere Gedanken?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	thoughtsChild: {
 		'de-urban': '<p>Können Tiere denken?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	thoughtsComp: {
 		'de-urban': '<p>Denken Tiere so wie Menschen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	feelingsAdult: {
 		'de-urban': '<p>Haben Tiere Gefühle?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	feelingsChild: {
 		'de-urban': '<p>Können Tiere fühlen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	feelingsComp: {
 		'de-urban': '<p>Fühlen Tiere so wie Menschen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	differenceAdult: {
 		'de-urban': '<p>Unterscheiden sich Menschen und Tiere?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	differenceChild: {
 		'de-urban': '<p>Sind Menschen anders als Tiere?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	intelligence: {
 		'de-urban': '<p>Wie schlau sind sie?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	pain: {
 		'de-urban': '<p>Wie sehr können sie Schmerzen fühlen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	feelingsRating: {
 		'de-urban': '<p>Wie sehr können sie sich glücklich,<br>traurig oder ängstlich fühlen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	similarity: {
 		'de-urban': '<p>Wie ähnlich sind sie Menschen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	notAtAll: {
 		'de-urban': '<p>Überhaupt nicht</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	aLittle: {
 		'de-urban': '<p>Ein bisschen</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	aMediumAmount: {
 		'de-urban': '<p>Mittel</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	aLotSmart: {
 		'de-urban': '<p>Sehr schlau</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	aLot: {
 		'de-urban': '<p>Sehr</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	aLotSimilar: {
 		'de-urban': '<p>Sehr ähnlich</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	ownershipChild: {
 		'de-urban': '<p>Haben Du und Deine Familie Tiere<br>oder gibt es Tiere bei euch zuhause?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	ownershipAdult: {
 		'de-urban':
 			'<p>Haben oder hatten du und deine Familie Tiere<br>bzw. gibt oder gab es Tiere bei dir zuhause?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	exposureChild: {
 		'de-urban': '<p>Wie oft triffst Du diese Tiere?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	exposureAdult: {
 		'de-urban':
 			'<p>Wie oft triffst du diese Tiere in deinem Alltag<br>bzw. hast sie getroffen?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	never: {
 		'de-urban': '<p>Nie</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	sometimes: {
 		'de-urban': '<p>Manchmal</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	often: {
 		'de-urban': '<p>Oft</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	almostEveryDay: {
 		'de-urban': '<p>Fast jeden Tag</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	meatFish: {
 		'de-urban': '<p>Isst Du Fleisch oder Fisch?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	eggCheeseMilk: {
 		'de-urban': '<p>Isst Du Eier, Käse oder trinkst Du Milch?</p>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 
 	thankYou: {
 		'de-urban': '<h1 class="heading">Vielen Dank!</h1>',
-		'na-rural': '',
-		'idk-rural': '',
-		'idj-urban': '',
-		'pe-rural': '',
 	},
 };
