c98f2d4d72ee5ae2277caae499da72033f298455
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-23 17:10:40 +0100

    Prevent audio/video break-out

--- src/procedure/sIntroduction.ts
+++ src/procedure/sIntroduction.ts
@@ -1,13 +1,17 @@
 import { gsap } from 'gsap';
-import { play, playPromise, stop } from '../util/audio';
+import { play, stop } from '../util/audio';
 import { swapSlides } from '../util/slideVisibility';
 import { getResponse } from '../util/getResponse';
 import _ from 'lodash';
 import { SvgInHtml } from '../types';
+import { sleep } from '../util/helpers';
 
 export default async () => {
 	// show slide
 	swapSlides(_.kebabCase(data.currentSlide));
+
+	const video = document.getElementById('player') as HTMLMediaElement;
+	const audio = document.getElementById('audio') as HTMLMediaElement;
 
 	const speaker = document.getElementById('link-si-speaker') as SvgInHtml;
 	const pinda = document.getElementById('player') as HTMLVideoElement;
@@ -18,11 +22,14 @@ export default async () => {
 
 	gsap.set([headphones, nextButton], { transformOrigin: '50% 50%', autoAlpha: 0 });
 
+	let playingTimeline = false;
 	speaker.addEventListener('click', () => {
+		gsap.set(speaker, { autoAlpha: 0 });
 		// timeline start
 		tl.to(pinda, {
 			onStart: () => {
 				pinda.src = `./cultures/${data.culture}/video/s-introduction.webm`;
+				playingTimeline = true;
 			},
 		})
 			.to(
@@ -60,11 +67,42 @@ export default async () => {
 				onStart: () => {
 					play(`./cultures/${data.culture}/audio/si-next-red.mp3`, 'link-si-headphones');
 				},
+				onComplete: () => {
+					playingTimeline = false;
+				},
 			});
 	});
 
-	await getResponse('link-si-next');
+	audio.addEventListener('play', () => {
+		console.log('audio started');
+		nextButton.style.pointerEvents = 'none';
+		gsap.set(nextButton, { autoAlpha: 0.25 });
+	});
+	video.addEventListener('play', () => {
+		console.log('video started');
+		nextButton.style.pointerEvents = 'none';
+		if (playingTimeline) {
+			gsap.set(nextButton, { autoAlpha: 0 });
+		} else {
+			gsap.set(nextButton, { autoAlpha: 0.25 });
+		}
+	});
+	audio.addEventListener('ended', () => {
+		console.log('audio ended');
+		nextButton.style.pointerEvents = 'auto';
+		gsap.to(nextButton, { autoAlpha: 1 });
+	});
+	video.addEventListener('ended', () => {
+		nextButton.style.pointerEvents = 'auto';
+		gsap.to(nextButton, { autoAlpha: 1 });
+		console.log('video ended');
+	});
+
+	await getResponse(nextButton.id);
 
 	// kill timeline animations
 	tl.kill();
+
+	stop();
+	await sleep(500);
 };
