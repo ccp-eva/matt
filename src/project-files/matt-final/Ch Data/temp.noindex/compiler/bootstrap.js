15e5d4c4dd479ad0043a8cc51e8bad8e05c967d5
Noemi Thiede <noemi_thiede@eva.mpg.de>
2023-03-29 17:38:53 +0200

    remove character animator project

--- src/project-files/matt-final/Ch Data/temp.noindex/compiler/bootstrap.js
+++
@@ -1,3 +0,0 @@
-var __bootStrapPath = _modulePath;
-var __compiledScene = _cwd + "/scene.json";
-runScript("/Applications/Adobe Character Animator 2023/Adobe Character Animator 2023.app/Contents/Resources/Runtime/animal/scripts/js/lib/engine.js")
