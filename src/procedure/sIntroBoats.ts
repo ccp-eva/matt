a11edafac6cac54777f917fdb2c8c39032cc6cc3
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-03-30 15:28:57 +0200

    Update video slides

--- src/procedure/sIntroBoats.ts
+++ src/procedure/sIntroBoats.ts
@@ -1,48 +1,73 @@
 import { gsap } from 'gsap';
-import _, { head } from 'lodash';
-import { SvgInHtml } from '../types';
-import { play, playPromise } from '../util/audio';
+import _ from 'lodash';
 import { sleep } from '../util/helpers';
 import { swapSlides } from '../util/slideVisibility';
 
 export default async () => {
+	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide), [2, 2]);
 	const pinda = document.getElementById('player') as HTMLVideoElement;
-	const headphones = document.getElementById('link-sib-headphones') as SvgInHtml;
+	const fsplayer = document.getElementById('fsplayer') as HTMLVideoElement;
 
-	gsap.set([pinda, headphones], { autoAlpha: 0 });
+	let isPlaying = true;
 
-	const tl = gsap.timeline();
+	[pinda, fsplayer].forEach((player) => {
+		player.addEventListener('play', () => {
+			isPlaying = true;
+		});
+		player.addEventListener('ended', () => {
+			isPlaying = false;
+		});
+	});
 
-	tl.to(pinda, {
+	gsap.set(pinda, { autoAlpha: 0 });
+
+	gsap.to(pinda, {
 		autoAlpha: 1,
 		duration: 2,
 		onStart: () => {
 			pinda.src = `./cultures/${data.culture}/video/sr-react2-intro-dilemmas.webm`;
 		},
-	}).to(pinda, {
-		autoAlpha: 0,
-		delay: 20,
 	});
 
-	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide), [2, 2]);
+	while (isPlaying) {
+		await sleep(100);
+	}
 
-	await sleep(23000);
-
-	const fsplayer = document.getElementById('fsplayer') as HTMLVideoElement;
-	gsap.set(fsplayer, { autoAlpha: 0 });
-	tl.to(fsplayer, {
-		display: 'block',
-		autoAlpha: 1,
-		duration: 2,
-		onStart: () => {
-			fsplayer.src = `./cultures/${data.culture}/video/s-intro-boats.mp4`;
-		},
-	}).to(fsplayer, {
+	gsap.to(pinda, {
 		autoAlpha: 0,
-		delay: 17,
 	});
 
-	await playPromise(`./cultures/${data.culture}/audio/s-intro-boats.mp3`);
+	isPlaying = true;
+	gsap
+		.timeline()
+		.to('#svg', {
+			backgroundColor: '#000',
+			duration: 1,
+		})
+		.to(fsplayer, {
+			autoAlpha: 1,
+			onStart: () => {
+				fsplayer.src = `./cultures/${data.culture}/video/s-intro-combined-720p.mp4`;
+			},
+		});
+
+	while (isPlaying) {
+		await sleep(100);
+	}
+
+	gsap
+		.timeline()
+		.to('#svg', {
+			backgroundColor: '#fff',
+			duration: 1,
+		})
+		.to(
+			fsplayer,
+			{
+				autoAlpha: 0,
+			},
+			'<'
+		);
 
 	await sleep(500);
 };
--- src/procedure/sResolveDilemma.ts
+++ src/procedure/sResolveDilemma.ts
@@ -1,7 +1,5 @@
 import { gsap } from 'gsap';
-import _, { head } from 'lodash';
-import { SvgInHtml } from '../types';
-import { play, playPromise } from '../util/audio';
+import _ from 'lodash';
 import { sleep } from '../util/helpers';
 import { swapSlides } from '../util/slideVisibility';
 
@@ -9,16 +7,45 @@ export default async () => {
 	swapSlides(_.kebabCase(data.currentSlide), _.kebabCase(data.previousSlide), [2, 2]);
 	const fsplayer = document.getElementById('fsplayer') as HTMLVideoElement;
 
-	gsap.set(fsplayer, { autoAlpha: 0 });
-	gsap.timeline().to(fsplayer, {
-		display: 'block',
-		autoAlpha: 1,
-		duration: 2,
-		onStart: () => {
-			play(`./cultures/${data.culture}/audio/resolve-dilemma.mp3`);
-			fsplayer.src = `./cultures/${data.culture}/video/s-resolve-dilemma.mp4`;
-		},
+	let isPlaying = true;
+
+	fsplayer.addEventListener('play', () => {
+		isPlaying = true;
+	});
+	fsplayer.addEventListener('ended', () => {
+		isPlaying = false;
 	});
 
-	await sleep(20000);
+	gsap
+		.timeline()
+		.to('#svg', {
+			backgroundColor: '#000',
+			duration: 1,
+		})
+		.to(fsplayer, {
+			autoAlpha: 1,
+			onStart: () => {
+				fsplayer.src = `./cultures/${data.culture}/video/s-resolve-dilemma-720p.mp4`;
+			},
+		});
+
+	while (isPlaying) {
+		await sleep(100);
+	}
+
+	gsap
+		.timeline()
+		.to('#svg', {
+			backgroundColor: '#fff',
+			duration: 1,
+		})
+		.to(
+			fsplayer,
+			{
+				autoAlpha: 0,
+			},
+			'<'
+		);
+
+	await sleep(500);
 };
