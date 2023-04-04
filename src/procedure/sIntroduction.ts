6613bac164f42fd4858212d62a127112b426e08b
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-04-04 15:08:05 +0200

    cleanup introduction

--- src/procedure/sIntroduction.ts
+++ src/procedure/sIntroduction.ts
@@ -26,83 +26,77 @@ export default async () => {
 	const tl = gsap.timeline();
 	const headphones = document.getElementById('link-si-headphones') as SvgInHtml;
 	const nextButton = document.getElementById('link-si-next') as SvgInHtml;
-
-	gsap.set([headphones, nextButton], { transformOrigin: '50% 50%', autoAlpha: 0 });
+	gsap.set([headphones, nextButton], { autoAlpha: 0 });
 
 	let playingTimeline = false;
-	speaker.addEventListener('click', () => {
-		if (!config.devmode) {
+	speaker.addEventListener('click', async () => {
+		if (!config.devmode.on) {
 			startFullscreen();
 		}
+		gsap.to(speaker, { autoAlpha: 0 });
+		playingTimeline = true;
+		play(`./cultures/${data.culture}/audio/si-next-red.mp3`, headphones.id);
 
-		gsap.set(speaker, { autoAlpha: 0 });
-		// timeline start
-		tl.to(pinda, {
-			onStart: () => {
-				pinda.src = `./cultures/${data.culture}/video/s-introduction.webm`;
-				playingTimeline = true;
-			},
-		})
-			.to(
-				headphones,
-				{
+		audio.addEventListener('play', () => {
+			nextButton.style.pointerEvents = 'none';
+			headphones.style.pointerEvents = 'none';
+			gsap.set([nextButton, headphones], { autoAlpha: 0.25 });
+		});
+		video.addEventListener('play', () => {
+			nextButton.style.pointerEvents = 'none';
+			headphones.style.pointerEvents = 'none';
+			if (playingTimeline) {
+				gsap.set([nextButton, headphones], { autoAlpha: 0 });
+			} else {
+				gsap.set([nextButton, headphones], { autoAlpha: 0.25 });
+			}
+		});
+		audio.addEventListener('ended', () => {
+			nextButton.style.pointerEvents = 'visible';
+			headphones.style.pointerEvents = 'visible';
+			gsap.to([nextButton, headphones], { autoAlpha: 1 });
+		});
+		video.addEventListener('ended', () => {
+			nextButton.style.pointerEvents = 'visible';
+			headphones.style.pointerEvents = 'visible';
+			gsap.to([nextButton, headphones], { autoAlpha: 1 });
+		});
+
+		pinda.src = `./cultures/${data.culture}/video/s-introduction.webm`;
+		pinda.onload = async () => {
+			await gsap
+				.timeline()
+				.to(headphones, {
+					autoAlpha: 1,
 					delay: 17,
 					duration: 0.5,
 					opacity: 1,
 					visibility: 'visible',
-				},
-				'<'
-			)
-			.to(headphones, {
-				filter: 'drop-shadow(0px 0px 14px #c4c4c4)',
-				delay: 1,
-				repeat: 2,
-				yoyo: true,
-				reversed: true,
-			})
-			.to(nextButton, {
-				delay: 5,
-				opacity: 1,
-				visibility: 'visible',
-			})
-			.to(nextButton, {
-				filter: 'drop-shadow(0px 0px 14px #a90707)',
-				delay: 1,
-				repeat: -1,
-				yoyo: true,
-				reversed: true,
-			})
-			.to(pinda, {
-				delay: 3,
-				autoAlpha: 0,
-				onStart: () => {
-					play(`./cultures/${data.culture}/audio/si-next-red.mp3`, 'link-si-headphones');
-				},
-				onComplete: () => {
-					playingTimeline = false;
-				},
-			});
-	});
-
-	audio.addEventListener('play', () => {
-		nextButton.style.pointerEvents = 'none';
-		gsap.set(nextButton, { autoAlpha: 0.25 });
-	});
-	video.addEventListener('play', () => {
-		nextButton.style.pointerEvents = 'none';
-		if (playingTimeline) {
-			gsap.set(nextButton, { autoAlpha: 0 });
-		} else {
-			gsap.set(nextButton, { autoAlpha: 0.25 });
-		}
-	});
-	audio.addEventListener('ended', () => {
-		nextButton.style.pointerEvents = 'auto';
-		gsap.to(nextButton, { autoAlpha: 1 });
-	});
-	video.addEventListener('ended', () => {
-		nextButton.style.pointerEvents = 'auto';
-		gsap.to(nextButton, { autoAlpha: 1 });
+				})
+				.to(headphones, {
+					filter: 'drop-shadow(0px 0px 14px #c4c4c4)',
+					delay: 1,
+					repeat: 2,
+					yoyo: true,
+					reversed: true,
+				})
+				.to(nextButton, {
+					autoAlpha: 1,
+					delay: 5,
+					opacity: 1,
+					visibility: 'visible',
+				})
+				.to(nextButton, {
+					filter: 'drop-shadow(0px 0px 14px #a90707)',
+					delay: 1,
+					repeat: -1,
+					yoyo: true,
+					reversed: true,
+					onComplete: () => {
+						playingTimeline = false;
+					},
+				});
+		};
 	});
 
 	await getResponse(nextButton.id);
