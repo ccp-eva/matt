046832b009ff6f8233d8bb9d9a4b336624da0691
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2023-04-08 14:21:25 +0200

    Default to defaultParams if provided parameter mismached defined ones

--- src/util/getUrlParameters.ts
+++ src/util/getUrlParameters.ts
@@ -14,19 +14,21 @@ export const getUrlParameters = () => {
 		if (!alphaNumeric.test(params.id)) {
 			Toastify({
 				escapeMarkup: false,
-				text: '<strong>Parameter Error</strong>: <small>Only alphanumeric ids are allowed!</small>',
+				text: `<strong>Parameter Error</strong>: <small>Only alphanumeric ids are allowed! Defaulting to: ${config.globals.defaultSubjectId}</small></small>`,
 				duration: 0,
-				className: 'toast-info',
+				className: 'toast-error',
 			}).showToast();
+			params.id = config.globals.defaultSubjectId;
 		}
 
-		if (params.id.length >= 20) {
+		if (params.id.length > 20) {
 			Toastify({
 				escapeMarkup: false,
-				text: '<strong>Parameter Error</strong>: <small>id too long!</small>',
+				text: `<strong>Parameter Error</strong>: <small>id too long! Defaulting to: ${config.globals.defaultSubjectId}</small>`,
 				duration: 0,
-				className: 'toast-info',
+				className: 'toast-error',
 			}).showToast();
+			params.id = config.globals.defaultSubjectId;
 		}
 	} else {
 		params.id = config.globals.defaultSubjectId;
@@ -37,10 +39,11 @@ export const getUrlParameters = () => {
 				escapeMarkup: false,
 				text: `🌏 <strong>Culture not found.</strong> <small>Your given URL paramter was not found within procedure objects in config.yaml. You either need to define the procedure, or check your URL parameter for typos.<br><br><b>Possible values: ${Object.keys(
 					config.procedure
-				).join(', ')}</b></small>`,
-				duration: 0,
+				).join(', ')}<br>Redirected to: ${config.globals.defaultCulture}</small></b></small>`,
+				close: true,
 				className: 'toast-error',
 			}).showToast();
+			params.culture = config.globals.defaultCulture;
 		}
 	} else {
 		params.culture = config.globals.defaultCulture;
@@ -49,11 +52,12 @@ export const getUrlParameters = () => {
 		if (params.agegroup !== 'adult' && params.agegroup !== 'child') {
 			Toastify({
 				escapeMarkup: false,
-				text: '<strong>Parameter Error</strong>: <small><code>agegroup</code> parameter can either be <code>adult</code> or <code>child</code></small>',
+				text: `<strong>Parameter Error</strong>: <small><code>agegroup</code> parameter can either be <code>adult</code> or <code>child</code></small><br><br>Defaulting to: <code>${config.globals.defaultAgeGroup}</code>`,
 				duration: 0,
 				className: 'toast-info',
 			}).showToast();
 		}
+		params.culture = config.globals.defaultCulture;
 	} else {
 		params.agegroup = config.globals.defaultAgeGroup;
 	}
@@ -66,11 +70,12 @@ export const getUrlParameters = () => {
 		) {
 			Toastify({
 				escapeMarkup: false,
-				text: '<strong>Parameter Error</strong>: <small><code>input</code> parameter can only be: <code>text</code>, <code>audio</code>, <code>userchoice-audio</code> or <code>userchoice-text</code></small>',
+				text: `<strong>Parameter Error</strong>: <small><code>input</code> parameter can only be: <code>text</code>, <code>audio</code>, <code>userchoice-audio</code> or <code>userchoice-text</code></small><br><br> Defaulting to <code>${config.globals.defaultInput}</code>`,
 				duration: 0,
 				className: 'toast-info',
 			}).showToast();
 		}
+		params.input = config.globals.defaultInput;
 	} else {
 		params.input = config.globals.defaultInput;
 	}
@@ -79,11 +84,12 @@ export const getUrlParameters = () => {
 		if (params.datatransfer !== 'both' && params.datatransfer !== 'server') {
 			Toastify({
 				escapeMarkup: false,
-				text: '<strong>Parameter Error</strong>: <small><code>datatransfer</code> parameter can only be: <code>both</code> or <code>server</code></small>',
+				text: `<strong>Parameter Error</strong>: <small><code>datatransfer</code> parameter can only be: <code>both</code> or <code>server</code></small><br><br> Defaulting to <code>${config.globals.defaultDataTransfer}</code>`,
 				duration: 0,
 				className: 'toast-info',
 			}).showToast();
 		}
+		params.datatransfer = config.globals.defaultDataTransfer;
 	} else {
 		params.datatransfer = config.globals.defaultDataTransfer;
 	}
